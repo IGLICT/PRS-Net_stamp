fc26ab7949539e4b64264910461b07e03e8cfba5
jwzxgy2007 <823951506@qq.com>
2021-02-22 11:15:10 +0800

    init

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ source ~/prsnet/bin/activate
 pip install -U pip
 
 # install necessary library
-pip install tensorflow-gpu==1.15.0
+pip install tensorflow==1.15.0
+pip install tensorboard==1.15.0
 pip install torch==1.7.1
 pip install torchsummary==1.5.1
+pip install scipy==1.6.1
 
 echo "installing sucessfully"
